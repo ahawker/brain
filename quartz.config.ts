7fbf5b802af553f0358ceaebf61b0fd3df75fc76
Andrew Hawker <andrew.r.hawker@gmail.com>
2024-07-22 22:09:22 -0700

    fix: title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🧠 Dump",
+    pageTitle: "🧠 Just Some Thoughts",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
